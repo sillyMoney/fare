9348177635e48079f8d602f6c21f6d02acca4492
sillyMoney <linxiaotian1986@gmail.com>
2017-06-17 00:17:35 +0800

    init project

---
+++ src/main/java/com/yushan/flight/fare/FareApplication.java
@@ -0,0 +1,12 @@
+package com.yushan.flight.fare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FareApplication.class, args);
+	}
+}
